ea3719c98dd6f6b60392253067ee5259a06b0aad
meiyi <myli@mail.fcu.edu.tw>
2022-05-03 16:14:37 +0800

    :sparkles: bilingual edition

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import { useTranslation } from "react-i18next";
+import { changeLanguage } from "i18next";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const { t, i18n } = useTranslation();
+   const [lang, setLang] = useState<any>("zh-TW");
+
+   const changeLanguage = () => {
+      switch (lang) {
+         case "zh-TW":
+            i18n.changeLanguage("en");
+            setLang("en");
+            break;
+
+         case "en":
+            i18n.changeLanguage("zh-TW");
+            setLang("zh-TW");
+            break;
+
+         default:
+            break;
+      }
+   };
+   return (
+      <div className="App">
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>{t("quickAdd.title")}</p>
+            <button
+               onClick={() => {
+                  changeLanguage();
+               }}
+            >
+               切換語言
+            </button>
+         </header>
+      </div>
+   );
 }
 
 export default App;
